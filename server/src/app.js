7b5372ad5083c5da735cec98683f810e049d5e06
Shailesh <mallicksailesh957@gmail.com>
2024-10-16 14:19:57 +0530

    Creating the server1

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,22 @@
-//Server
-
 const express = require('express')
 const app = express();
-
 const port =8000;
 
-app.get('/',(req,res)=>{
-    res.status(200).json({message:'Hello From Dev Tinder !'})
+
+
+app.get('/dashboard',(req,res)=>{
+    res.send('<h1>Hello from the Dev-Tinder Dashboard</h1>')
 })
 
+app.get('/',(req,res)=>{
+    res.end("<h1>Hello fron the Dev-Tinder Homepage</h1>")
+})
+
+app.use((req,res)=>{
+    res.send('Hello from the server')
+})
+
+
 app.listen(port,()=>{
-    console.log(`App running on ${port}`)
+    console.log(`Dev-Tinder server is running on ${port}`)
 })
