65a3c9150510a5bf115b067db22f10fde5bc5a8c
Shailesh <mallicksailesh957@gmail.com>
2024-11-14 15:29:14 +0530

    dynamic_data-signup API

--- server/src/app.js
+++ server/src/app.js
@@ -4,27 +4,43 @@ const port =8000;
 const connectDB =require("./config/database")
 const UserModel = require('./models/user')
 
+
+app.use(express.json()); //for parsing the JSON OBJECTS
+
 //API
 app.post('/signup',async(req,res)=>{
-    //Logic to add data into the database
-    const userObj ={
-        firstName:"Shailesh Sundar",
-        lastName:"Mallick",
-        emailId:"mallicksailesh957@gmail.com",
-        password:"123@sd"
-    }
-    //creating  a new instance of the userModel
-    const user = new UserModel(userObj);
 
+    const user = new UserModel(req.body)
     try{
-    await user.save(); //Data is saved in to the database
+    await user.save()
 
-    res.send("User Added successfully")
-
-    } catch(err){
-        res.status(400).send("Error saving it to the database:"+err.message);
+    res.send("User Added successfully in our database")
     }
-})
+    catch(err){
+        res.status(400).send("Error in adding user to the database")
+    }
+
+
+    //Creating a dummy object
+    // const userObj={
+    //     firstName:"Virat",
+    //     lastName:"Kohli",
+    //     emailId:"mallicksailesh957@gmail.com",
+    //     password:"vil12@"
+    // }
+
+    // //Creating  a new instance of that userModel
+    // try{
+    // const user = new UserModel(userObj)
+
+    // await user.save();
+
+    // res.send("User Added successfully")
+
+    // }catch(err){
+    //     res.status(400).send("Error adding data to the database"+err.message)
+    //  }
+    })
 
 
 
