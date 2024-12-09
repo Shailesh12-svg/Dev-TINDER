ebced72ad8c92226b94606cbb096aebf3887b1eb
Shailesh <mallicksailesh957@gmail.com>
2024-12-09 22:22:28 +0530

    profile ignored/accepted API's

--- server/src/models/connectionRequest.js
+++ server/src/models/connectionRequest.js
@@ -23,6 +23,14 @@ const connectionRequestSchema = new mongoose.Schema({
     timestamps:true,
 });
 
+connectionRequestSchema.pre("save",function(next){
+    const connectionRequest =this;
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("Cannot send connection request to yourself..")
+    }
+    next();
+})
+
 //Creating a model
 
 const ConnectionRequest = new mongoose.model('ConnectionRequest',connectionRequestSchema);
--- server/src/routes/connectionRequest.js
+++ server/src/routes/connectionRequest.js
@@ -2,7 +2,7 @@ const express = require('express')
 const {userAuth} = require("../middlewares/auth");
 const connectionRequestRouter = express.Router();
 const ConnectionRequest = require('../models/connectionRequest')
-
+const UserModel = require('../models/user')
 //CONNECTION REQUEST API {Protected using userAuth}
 
 connectionRequestRouter.post("/request/send/:status/:toUserId",userAuth,async(req,res)=>{
@@ -16,11 +16,47 @@ connectionRequestRouter.post("/request/send/:status/:toUserId",userAuth,async(re
         //To get me the toUserId  ...
         const toUserId = req.params.toUserId;
         const status = req.params.status;
+        //3rd Problem validate the toUserId
+        // Ky kro ki joh toUserId ky woh database exist krta hai ye dekh lo simple
+
+        const isToUserIdValid = await UserModel.findById(toUserId)
+
+        if(!isToUserIdValid){
+           return res.status(400).json({
+                message:"The toUser does not exist in the database"
+            })
+        }
+
+
+
+        const allowedStatus = ['ignored','interested'];
+        //1st problem
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({
+                message:"Invalid status type"
+            })
+        }
        
         const connection = new ConnectionRequest({
             fromUserId,toUserId,status
         })
-        
+
+        //2nd problem existing connection request
+
+        const existingConnectionRequest = await ConnectionRequest.findOne({
+            //USE DIMAG 
+
+            $or:[
+                 //{fromUserId,fromUserId}, // a-> a same connection bug
+                {fromUserId,toUserId}, //a->b again
+                {fromUserId:toUserId,toUserId:fromUserId} //b->a reverse
+            ],
+        });
+
+        if(existingConnectionRequest){
+            return res.status(400).json({message:"Connection request already exist.."})
+        }
+
         const final = await connection.save();
 
         if(final){
@@ -29,7 +65,7 @@ connectionRequestRouter.post("/request/send/:status/:toUserId",userAuth,async(re
                 final,
             })
         }
-
+        
 
     }catch(err){
         res.status(400).send("ERROR ...."+err.message);
