12e7356af2571333ba16aa3ab753d18f9e52121d
Shailesh <mallicksailesh957@gmail.com>
2024-12-14 12:59:30 +0530

    changes

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
-
+const Users = require('../models/user');
 const userRouter = express.Router();
 
 userRouter.get('/user/requests/received',userAuth,async (req,res)=>{
@@ -15,9 +15,9 @@ userRouter.get('/user/requests/received',userAuth,async (req,res)=>{
         status :"interested",
     
     }).populate("fromUserId",["firstName","lastName","photoUrl","age","gender","about"])
-
-    if(!connectionRequest){
-        res.status(400).json({message:"No conncetion request there..."})
+    
+    if(connectionRequest.length===0){
+        res.status(400).json({message:"No connection request there..."})
     }else{
         res.status(200).json({message:"Pending connection requests: ",connectionRequest})
     }
@@ -53,5 +53,43 @@ userRouter.get('/user/connections',userAuth,async(req,res)=>{
 }
     
 })
+
+//toUGH ONE
+
+userRouter.get('/user/feed',userAuth,async(req,res)=>{
+    try{
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page)||1;
+    let limit = parseInt(req.query.limit)||10;
+    limit = limit>50?50:limit;
+
+    const skip =(page-1)*limit;
+    //Find me the connection request to whom we sended the connection or recieved..
+    const connectionRequest = await ConnectionRequest.find({
+        $or:[
+            {fromUserId:loggedInUser},
+            {toUserId:loggedInUser},
+        ]
+    }).select("fromUserId toUserId");
+
+    const hideUserFeed = new Set();
+
+    connectionRequest.forEach(req=>{
+
+        hideUserFeed.add(req.fromUserId.toString());
+        hideUserFeed.add(req.toUserId.toString());
+    });
+
+    const users = await Users.find({
+        $and:[
+            {_id:{$nin:Array.from(hideUserFeed)}},
+            {_id:{$ne:loggedInUser._id},
+        }]
+    }).select("firstName lastName photoUrl age gender about").skip(skip).limit(limit)
+    res.json(users);
+}catch(err){
+    res.status(400).json({message:err.message})
+}
+});
 
 module.exports = userRouter;
