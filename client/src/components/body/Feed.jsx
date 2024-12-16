822727a0a1a6387ee507dd194897e9e2a66cee7e
Shailesh <mallicksailesh957@gmail.com>
2024-12-16 21:26:37 +0530

    UserCard on Feed

--- client/src/components/body/Feed.jsx
+++ client/src/components/body/Feed.jsx
@@ -1,8 +1,38 @@
+import axios from "axios"
+import { BASE_URL } from "../../utils/constants"
+import { useDispatch, useSelector } from "react-redux"
+import { addFeed } from "../../utils/feedSlice";
+import { useEffect } from "react";
+import UserCard from "./UserCard";
 
 
 const Feed = () => {
+  const dispatch = useDispatch();
+  const feed =useSelector((store)=>store.feed)
+  const getFeed = async()=>{
+  if(feed)return;
+    try{
+
+      const res = await axios.get(BASE_URL+"/user/feed",{withCredentials:true,})
+      dispatch(addFeed(res.data))
+      
+    }catch(err){
+      return res.status(400).json({message:"Oops something went wrong.."})
+    }
+  }
+
+  useEffect(()=>{
+    getFeed();
+  },[])
+
+
+
   return (
-    <div>Feed</div>
+    feed&&(
+    <div className="flex justify-center  my-10">
+      <UserCard user={feed[0]}/>
+    </div>
+    )
   )
 }
 
